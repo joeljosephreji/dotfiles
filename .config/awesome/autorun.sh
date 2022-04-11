1a151d9deaed2fe4456b65c684ec1ab72855b952
joeljosephreji <joeljosephreji@gmail.com>
2022-04-12 00:24:37 +0530

    added awesome wm config

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep -f "$1" ;
+  then
+    $@&
+  fi
+}
+
+run nm-applet &
+run cbatticon &
+run flameshot &
+run nitrogen --restore &
+run picom --experimental-backends &
+run parcellite &
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+run discord --start-minimized &
+run kdeconnect-indicator &
+run blueman-applet &
